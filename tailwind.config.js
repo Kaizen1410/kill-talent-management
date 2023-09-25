d9f675eb77a96e71291f02665565b079125af5c0
Ahmad Muhajir <amuhajir.syamlan@gmail.com>
2023-09-26 00:10:25 +0700

    fix line-height & layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,36 @@
 module.exports = {
     content: ["./*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        fontSize: {
+          '48': [
+            '48px', {
+              lineHeight: 'normal',
+              letterSpacing: '4.8px'
+            }
+          ],
+          '28': [
+            '28px', {
+              lineHeight: 'normal'
+            }
+          ],
+          '24': [
+            '24px', {
+              lineHeight: 'normal'
+            }
+          ],
+          '16': [
+            '16px', {
+              lineHeight: 'normal'
+            }
+          ],
+          '14': [
+            '14px', {
+              lineHeight: '150%'
+            }
+          ],
+        }
+      },
     },
     plugins: [require("daisyui")],
   }
