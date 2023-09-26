cacbc0f4179c34360bc5f57ebdac59939c355654
Kaizen1410 <lutfifahrezi04@gmail.com>
2023-09-26 10:27:46 +0700

    portofolio page

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,9 @@ module.exports = {
               lineHeight: '150%'
             }
           ],
+        },
+        fontFamily: {
+          'Libre-Baskerville': ['Libre Baskerville']
         }
       },
     },
